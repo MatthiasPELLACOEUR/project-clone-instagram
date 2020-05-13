a4119b7584c6868779b0d8e389f1189542519d94
VEROFOREST <xvforest@orange.fr>
2020-05-13 10:36:34 +0200

    added php to add commentary

--- photos/add-commentary.php
+++ photos/add-commentary.php
@@ -1,3 +1,41 @@
+<?php
+// connect bdd
+require '../connection.php';
+// renommer les variables
+if (isset($_POST['nickname']) and isset($_POST['photosid']) and isset ($_POST['comment']));
+    $nickname = htmlspecialchars($_POST['nickname']);
+    // var_dump($nickname);
+    $photosid = $_POST['photosid'];
+    // var_dump($photosid);exit;
+    $comment  =  $_POST['comment'];
+    $dateTime = date('Y-m-d H:i:s');
+    $ipadress = getIp();
+    // association  users_id et nickname
+    $userstatement=$bdd->prepare('SELECT * FROM users WHERE nickname=?');
+    $userstatement->execute([$_POST['nickname']]);
+    $user=$userstatement->fetch(PDO :: FETCH_ASSOC);
+    $usersid=$user['id'];
+    var_dump($usersid);
+    // association id photos  et "input type hidden=photos id
+    $photosstatement=$bdd->prepare('SELECT * FROM photos WHERE id=?');
+    $photosstatement->execute([$_POST['photosid']]);
+    $photo=$photosstatement->fetch(PDO :: FETCH_ASSOC);
+    $photosid=$photo['id'];
+    var_dump($photosid);
+    // inserer les commentaires fait par le user et la photo concernée
+    $requestinsertcomment=$bdd->PREPARE('INSERT INTO comments (photos_id,users_id,comment,created_at,ip_address) 
+                                         VALUES(?,?,?,?,?)');
+    $requestinsertcomment->EXECUTE([
+                    $photosid,
+                    $usersid,
+                    $comment,
+                    $dateTime,
+                    $ipadress,
+                    ]);
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,26 +55,36 @@
     </header>
     <section>
     <div class="container">
-    <div class="row">
-        <form class="col s12">
-            <div class="row">
-                <div class="input-field col s4">
-                <i class="material-icons prefix">account_circle</i>
-                <input id="icon_prefix" type="text" class="validate white-text" placeholder="nickname">
-                
-            </div>
-            
         
-            <div class="row col s12">    
-                <div class="input-field col white-text s12">
-                <textarea id="textarea1" class="materialize-textarea" placeholder="Let a Comment"></textarea>
+            <!-- nickname qui envoie le comm=user connect remplacé par la variable -->
+            <form action="../photos/add-commentary.php" method="POST" class="formaddcomm col s12 m8 offset -m2">
+                <div class="row">
+                    <!-- pour récupérer l'url de la photo cliquée! -->
+                    <div>
+                    <input type="hidden" name="photosid" id="photosid" value="1">
+                    </div>
+
+                    <div class="input-field col s4 offset -s4">
+                    <i class="material-icons prefix">account_circle</i>
+                    <input id="icon_prefix" type="text" name="nickname" class="validate white-text" placeholder="nickname">
+                    </div>
+                </div>    
+                <!-- commentaire concernant la photo ou on a cliqué -->
+            
+                <div class="row col s12 m10">    
+                    <div class="input-field col white-text s8">
+                        <textarea id="textarea1" name ="comment" class="materialize-textarea white-text" placeholder="Let a Comment"></textarea>
+                    </div>
+                    
                 </div>
-            </div>
-    </form>
-  </div>
-        </form>
+                <button class="btn waves-effect waves-light blue-grey lighten-3 col s1 m2" type="submit" name="action">Submit
+                    <i class="material-icons right">send</i>
+                </button>
+                    
+            </form>
         
     </div>    
+        
     </section>
     <footer>
         <?php
