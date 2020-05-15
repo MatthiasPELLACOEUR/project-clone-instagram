948fc751fede2aa5b29b6c8c91976a0543c6eb92
MatthiasPELLACOEUR <matthias.pellacoeur@hotmail.com>
2020-05-15 09:45:32 +0200

    add-photo start responsive

--- profile/add-photos.php
+++ profile/add-photos.php
@@ -4,16 +4,20 @@ session_start();
 require '../partials/header.php';
 ?>
 <section class="container">
-    <form action="upload-photos.php" method="post" enctype="multipart/form-data">
-        <input type="hidden" name="user_id" value="<?=$_SESSION['id']?>">
-        <label for="photo">Select image to upload:</label>
-        <input type="file" name="fileToUpload" class="input-yellow" id="fileToUpload"><br>
-        <label for="caption">Caption :</label>
-        <input type="text" name="caption" class="input-pink">
-        <button class="btn waves-effect waves-light purple right" type="submit" name="submit">Upload Image
-            <i class="material-icons right">chevron_right</i>
-        </button>
-    </form>
+    <div class="row">
+        <div class="col s12 offset-s1 m6 offset-m2 l5 offset-l3">
+            <form action="upload-photos.php" method="post" enctype="multipart/form-data">
+                <input type="hidden" name="user_id" value="<?=$_SESSION['id']?>">
+                <label for="photo">Select image to upload:</label>
+                <input type="file" name="fileToUpload" class="input-yellow" id="fileToUpload"><br>
+                <label for="caption">Caption :</label>
+                <input type="text" name="caption" class="input-pink">
+                <button class="btn waves-effect waves-light purple right" type="submit" name="submit">Upload Image
+                    <i class="material-icons right">chevron_right</i>
+                </button>
+            </form>
+        </div>
+    </div>
 </section>
 
 <footer>
